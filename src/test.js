fcefa5de8ee9cdbb3c1afe33984758d67af0a31f
Everton Candelone <evertoncandelone190@gmail.com>
2023-03-27 16:21:48 -0300

    commit ts

---
+++ src/test.js
@@ -0,0 +1,11 @@
+"use strict";
+function calculaArea(base, altura) {
+    return base * altura;
+}
+console.log(calculaArea(10, 34));
+function saldacao(nome, sobrenome) {
+    nome = "Everton";
+    sobrenome = "Cadelone";
+    return ('Olá' + nome + " " + sobrenome);
+}
+console.log(calculaArea);
---
+++ src/test.ts
@@ -0,0 +1,19 @@
+function calculaArea(base: number, altura: number): number {
+    return base * altura;
+}
+console.log(calculaArea(10, 34));
+
+function saldacao(nome: string, sobrenome: string): string {
+    nome = "Everton";
+    sobrenome = "Cadelone";
+    return ('Olá' + nome + " " + sobrenome);
+}
+
+console.log(calculaArea);
+
+
+
+
+
+
+
